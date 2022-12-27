035466e132b5d7585fb74ed225bda3f97b9eaed6
Chris F <CF-public@gmx.net>
2022-12-27 15:42:27 +0100

    Initial commit

---
+++ HarmonyPatches.cs
@@ -0,0 +1,71 @@
+﻿using HarmonyLib;
+using Mono.Cecil.Cil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem.CampaignBehaviors;
+using TaleWorlds.CampaignSystem.GameComponents;
+using TaleWorlds.CampaignSystem.Settlements;
+using TaleWorlds.CampaignSystem.ViewModelCollection;
+using TaleWorlds.CampaignSystem.ViewModelCollection.Inventory;
+using TaleWorlds.Core;
+using TaleWorlds.Core.ViewModelCollection;
+using TaleWorlds.Core.ViewModelCollection.Information;
+using TaleWorlds.Library;
+
+namespace CustomPatches
+{
+	[HarmonyPatch(typeof(Crafting), "SwitchToPiece")]
+	internal static class Patch_Crafting_SwitchToPiece
+	{
+		[HarmonyTranspiler]
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			var include = false;
+			foreach (var instruction in instructions)
+			{
+				if (include)
+					yield return instruction;
+				else if (instruction.opcode == OpCodes.Callvirt
+					&& instruction.operand is MethodInfo methodInfo
+					&& methodInfo.Name == "SetScale")
+					include = true;
+			}
+		}
+	}
+
+	[HarmonyPatch(typeof(CraftingCampaignBehavior), "IsOpened")]
+	internal static class Patch_CraftingCampaignBehavior_IsOpened
+	{
+		[HarmonyPostfix]
+		public static void Postfix(ref bool __result)
+		{
+			if (CustomPatches.Settings.UnlockAllParts)
+				__result = true;
+		}
+	}
+
+	[HarmonyPatch(typeof(DefaultSettlementEconomyModel), "GetDailyDemandForCategory")]
+	internal static class Patch_DefaultSettlementEconomyModel_GetDailyDemandForCategory
+	{
+		[HarmonyPostfix]
+		public static void Postfix(ref float __result)
+		{
+			__result *= CustomPatches.Settings.TradeDemandModifier;
+		}
+	}
+
+	[HarmonyPatch(typeof(DefaultPartySpeedCalculatingModel), "CalculateBaseSpeedForParty")]
+	internal static class Patch_DefaultPartySpeedCalculatingModel_CalculateBaseSpeedForParty
+	{
+		[HarmonyPostfix]
+		public static void Postfix(ref float __result)
+		{
+			__result *= CustomPatches.Settings.PartySpeedModifier;
+		}
+	}
+}
---
+++ MCMSettings.cs
@@ -0,0 +1,92 @@
+﻿using HarmonyLib;
+using MCM.Abstractions.Attributes;
+using MCM.Abstractions.Attributes.v2;
+using MCM.Abstractions.Base.Global;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Extensions;
+using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.CampaignSystem.Roster;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+
+namespace CustomPatches
+{
+	public class MCMSettings : AttributeGlobalSettings<MCMSettings>
+	{
+		public override string Id => "CustomPatches";
+		public override string DisplayName => "Custom Patches";
+		public override string FolderName => "CustomPatches";
+		public override string FormatType => "json";
+
+		[SettingPropertyBool(
+			"Unlock all smithing parts",
+			RequireRestart = false,
+			HintText = "Unlock all smithing parts",
+			Order = 0)]
+		[SettingPropertyGroup(
+			"Settings",
+			GroupOrder = 0)]
+		public bool UnlockAllParts { get; set; } = false;
+
+		[SettingPropertyButton(
+			"Give me Everything",
+			RequireRestart = false,
+			HintText = "HALP!",
+			Content = "HIT ME!",
+			Order = 1)]
+		[SettingPropertyGroup(
+			"Settings",
+			GroupOrder = 0)]
+		public Action AllEquipmentButton { get; set; } = () => 
+		{
+			var mainParty = Campaign.Current?.MainParty;
+			if (mainParty != null)
+			{
+				foreach (var item in Items.All)
+				{
+					if (!item.IsAnimal && !item.IsFood && !item.IsTradeGood && item.Culture == mainParty.LeaderHero.Culture /*&& !mainParty.ItemRoster.Any(ir => ir.EquipmentElement.Item == item)*/)
+					{
+						FileLog.Log($"Adding '{item.Name}'");
+						mainParty.ItemRoster.Add(new ItemRosterElement(item, 1));
+					}
+				}
+				InformationManager.DisplayMessage(new InformationMessage($"Done!", new Color(0f, 1f, 0f)));
+			}
+			else
+				InformationManager.DisplayMessage(new InformationMessage("Game not running", new Color(1f, 0f, 0f)));
+		};
+
+
+		[SettingPropertyFloatingInteger(
+			"Party Speed Modifier",
+			0.01f,
+			100f,
+			"0.00",
+			RequireRestart = false,
+			HintText = " [Native: 1.00]",
+			Order = 2)]
+		[SettingPropertyGroup(
+			"Settings",
+			GroupOrder = 0)]
+		public float PartySpeedModifier { get; set; } = 1f;
+
+
+		[SettingPropertyFloatingInteger(
+			"Trade Demand Modifier",
+			0.01f,
+			100f,
+			"0.00",
+			RequireRestart = false,
+			HintText = " [Native: 1.00]",
+			Order = 3)]
+		[SettingPropertyGroup(
+			"Settings",
+			GroupOrder = 0)]
+		public float TradeDemandModifier { get; set; } = 1f;
+	}
+}
